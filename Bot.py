97a18784aedf14d24c27baa343de6b9ba3448d2d
Exly <Exly788@gmail.com>
2026-03-08 13:01:43 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -9,7 +9,7 @@ import psutil
 import io
 import urllib.request
 # test
-
+print("hii")
 if sys.stdout.encoding != "utf-8":
     sys.stdout.reconfigure(encoding="utf-8", errors="replace")
 if sys.stderr.encoding != "utf-8":
